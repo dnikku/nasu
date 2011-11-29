954c6eae43ddd8639ebd7ae85414756050a7d7a1
nicu dascalu <dnikku@vera>
2011-11-29 06:56:11 +0100

    add support for multiple start file paths

--- media_files.py
+++ media_files.py
@@ -3,11 +3,13 @@ from os import walk, path
 import mimetypes
 
 
-def get_root_path():
-    root_path = 'E:\downloads\utorrent\completed'
+def get_root_paths():
     if sys.platform.startswith('linux'):
-        root_path = '..';
-    return root_path
+        return ['..'];
+    return [
+        'E:\downloads\utorrent\completed',
+        'E:\\mmedia\\audio\\germana',
+        ]
 
 ignore_files = [
     '.txt',
@@ -18,24 +20,26 @@ ignore_files = [
     '.db', '.torrent',
     ]
 
-def dir_files(start_from=get_root_path()):
-
-    for root, dirs, files in walk(start_from):
-        if '.git' in root or 've' in root:
-            pass
-        elif root.endswith('VIDEO_TS'):
-            yield {'name': path.dirname(root),
-                   'path': path.abspath(root),
-                   'mime': 'dvd'
-                   }
-        else:
-            for name in files:
-                if any(filter(lambda x: name.endswith(x), ignore_files)):
-                    print 'skip file: "%s"' % name
-                else:
-                    print 'add file: "%s"' % name
-                    file_path = path.abspath(path.join(root, name))
-                    yield {'name': name,
-                           'path': file_path,
-                           'mime': path.splitext(name)[1],
-                           }
+def dir_files(roots=get_root_paths()):
+    for start_from in roots:
+        print start_from
+        for root, dirs, files in walk(start_from):
+            print root
+            if '.git' in root or 've' in root:
+                pass
+            elif root.endswith('VIDEO_TS'):
+                yield {'name': path.dirname(root),
+                       'path': path.abspath(root),
+                       'mime': 'dvd'
+                       }
+            else:
+                for name in files:
+                    if any(filter(lambda x: name.endswith(x), ignore_files)):
+                        print 'skip file: "%s"' % name
+                    else:
+                        print 'add file: "%s"' % name
+                        file_path = path.abspath(path.join(root, name))
+                        yield {'name': name,
+                               'path': file_path,
+                               'mime': path.splitext(name)[1],
+                               }
